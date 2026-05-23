2078328acdd24deee0a80fea9683689ffa07c182
ThreeSprings <lylahaws@gmail.com>
2026-05-23 03:25:05 +0000

    change

--- script.js
+++ script.js
@@ -21,18 +21,32 @@ function render(list){
   list.forEach(p=>{
     const card = document.createElement('article');
     card.className = 'card';
-    // random width between 180 and 340px, random image height between 80 and 220px
-    const w = Math.floor(Math.random() * (340 - 180 + 1)) + 180;
-    const h = Math.floor(Math.random() * (220 - 80 + 1)) + 80;
-    card.style.width = w + 'px';
-    card.innerHTML = `
-      <div class="product-image" style="height:${h}px">${p.name.split(' ')[0]}</div>
-      <h4 class="product-title">${p.name}</h4>
-      <div class="product-meta">
-        <div class="price">${p.price}</div>
-        <a class="btn" href="mailto:info@threesprings.example?subject=Order%20request%20for%20${encodeURIComponent(p.name)}&body=Hi,%0A%0AI'd%20like%20to%20order%20%3A%20${encodeURIComponent(p.name)}.%0AQuantity:%20%0APickup%20or%20Delivery:%20%0A%0AThanks,">Buy</a>
-      </div>
-    `;
+    // For small devices make cards full width with a comfortable image height.
+    if (window.innerWidth <= 600) {
+      card.style.width = '100%';
+      const h = 120;
+      card.innerHTML = `
+        <div class="product-image" style="height:${h}px">${p.name.split(' ')[0]}</div>
+        <h4 class="product-title">${p.name}</h4>
+        <div class="product-meta">
+          <div class="price">${p.price}</div>
+          <a class="btn" href="mailto:info@threesprings.example?subject=Order%20request%20for%20${encodeURIComponent(p.name)}&body=Hi,%0A%0AI'd%20like%20to%20order%20%3A%20${encodeURIComponent(p.name)}.%0AQuantity:%20%0APickup%20or%20Delivery:%20%0A%0AThanks,">Buy</a>
+        </div>
+      `;
+    } else {
+      // random width between 180 and 340px, random image height between 80 and 220px
+      const w = Math.floor(Math.random() * (340 - 180 + 1)) + 180;
+      const h = Math.floor(Math.random() * (220 - 80 + 1)) + 80;
+      card.style.width = w + 'px';
+      card.innerHTML = `
+        <div class="product-image" style="height:${h}px">${p.name.split(' ')[0]}</div>
+        <h4 class="product-title">${p.name}</h4>
+        <div class="product-meta">
+          <div class="price">${p.price}</div>
+          <a class="btn" href="mailto:info@threesprings.example?subject=Order%20request%20for%20${encodeURIComponent(p.name)}&body=Hi,%0A%0AI'd%20like%20to%20order%20%3A%20${encodeURIComponent(p.name)}.%0AQuantity:%20%0APickup%20or%20Delivery:%20%0A%0AThanks,">Buy</a>
+        </div>
+      `;
+    }
     productsEl.appendChild(card);
   });
 }
